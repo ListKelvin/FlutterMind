d3803d52965747cec49c2d5c3e266d94b63d5395
Phạm Công Minh <phamm5687@gmail.com>
2024-05-21 19:07:31 +0700

    create a random word and favorite app

--- README.md
+++ README.md
@@ -28,6 +28,27 @@ samples, guidance on mobile development, and a full API reference.
 * Every `build` method must return ** a widget** or (more typically) **a nested tree of widgets**. In this case, the top-level widget is `Scaffold`.
 * Column is one of the most basic layout widgets in Flutter. It takes any number of children and puts them in a column from top to bottom.
 
+Separate component `MyHomePage` is extracted into a new widget, `GeneratorPage`.
+
+- Navigation: Widget `SafeArea` ensures that its child is not obscured by a hardware notch or a status bar.
+- Expanded widget: let you express layouts where some **children take only as much space as they need** (SafeArea, in this case) and other widgets should take **as much of the remaining room as possible** (Expanded, in this case).
+- The underscore (\_) at the start of \_MyHomePageState makes that class private and is enforced by the compiler. If you want to know more about privacy in Dart, and other topics
+- there is no route in flutter. We see all thing and change all thing With Widget
+- `Wrap` is a widget similar to `Row` or `Column` that automatically **wraps children to the next "line"** (called "run") when there isn't enough vertical or horizontal space.
+
+- Flutter works with **logical pixels** as a unit of length. They are also sometimes called **device-independent pixels**. A padding of **8 pixels** is visually the same regardless of whether the app is running on an old low-res phone or a newer ‘retina' device. There are roughly 38 logical pixels per centimeter, or about 96 logical pixels per inch, of the physical display.
+- `LayoutBuilder`. It lets you change your widget tree depending on how much available space you have.
+  LayoutBuilder's builder callback is called every time the constraints change
+
+### How StateWidgets work:
+
+- **Mutable State**: Unlike stateless widgets that display a fixed UI based on their properties, stateful widgets have mutable state. This state can be updated throughout the widget's lifetime.
+- **State Object**: The stateful widget itself doesn't directly manage the state. Instead, it creates a separate State object class. This state object holds the actual data that can be changed.
+- **Build Method**: Both the StatefulWidget and its State object have a build method. The StatefulWidget's build method typically creates the state object and defines the overall structure of the widget. The State object's build method, on the other hand, uses the current state data to build the UI.
+- **Updating State**: When the state needs to be changed, the State object calls a special method named setState. This tells Flutter that the state has changed, and it triggers a rebuild of the widget using the State object's build method again. This ensures the UI reflects the latest state.
+
 ## Questions
 
+- what is widget?
+  - Widgets are the elements from which you build every Flutter app. As you can see, even the app itself is a widget. Everything in a Flutter app is a widget
 - StatelessWidget versus StatefulWidget?
--- lib/main.dart
+++ lib/main.dart
@@ -44,44 +44,132 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+//The underscore (_) at the start of _MyHomePageState makes that class private and is enforced by the compiler.
+//If you want to know more about privacy in Dart, and other topics
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+
+      case 1:
+        page = FavoritesPage();
+
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+//LayoutBuilder's builder callback is called every time the constraints change
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  print('selected: $value hello from minh');
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+
     IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
     } else {
       icon = Icons.favorite_border;
     }
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: () => {appState.toggleFavorite()},
-                    icon: Icon(icon),
-                    label: Text("favorite")),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
-            ),
-          ],
-        ),
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next Minh nè'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return Center(
+        child: ListView(
+      children: appState.favorites
+          .map((e) => ListTile(
+              leading: Icon(Icons.favorite), title: Text(e.asCamelCase)))
+          .toList(),
+    ));
   }
 }
 
@@ -108,7 +196,7 @@ class BigCard extends StatelessWidget {
         child: Text(
           pair.asLowerCase,
           style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
+          semanticsLabel: "${pair.first} ${pair.second} hello",
         ),
       ),
     );
